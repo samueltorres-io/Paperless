1f2172253e9159a654e0c2e40527a7455a92a805
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-12 13:48:39 -0300

    Add constructor and methods to Task class for title update and task completion

--- Modules/ToDo/Task.cs
+++ Modules/ToDo/Task.cs
@@ -7,6 +7,25 @@ public class Task
     public bool IsComplete { get; private set; }
     public int Priority { get; private set; }
 
-    
+    public Task(string title, string description, int priority)
+    {
+        Title = title;
+        Description = description;
+        IsComplete = false;
+        Priority = priority;
+    }
+
+    public void UpdateTitle(string title)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be empty!");
+
+        Title = title;
+    }
+
+    public void CompleteTask()
+    {
+        IsComplete = true;
+    }
 
 }
