75bbc18c0599ed965b22baab6f8714deaf9bb214
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-12 16:02:56 -0300

    Add CreateTask method to TodoManager for task creation and persistence

--- Modules/ToDo/TodoManager.cs
+++ Modules/ToDo/TodoManager.cs
@@ -12,6 +12,21 @@ public class TodoManager : ITaskManager
         _repository = repository;
     }
 
-    
+    /* Cria e persiste uma nova tarefa */
+    public TodoTask CreateTask(string title, string description, int priority)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title is required!");
+
+        //todo: Add validaçaõ de description por tamanho de texto e as validações de validade
+
+        var item = new TodoTask(title, description, priority);
+
+        var tasks = _repository.LoadAll();
+        tasks.Add(item);
+        _repository.SaveAll(tasks);
+
+        return item;
+    }
 
 }
