254a694274dc4d6cc8b6bb144f845784e22086f0
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-12 13:29:48 -0300

    Add Task.cs file to define the structure of to-do items with properties for Title, Description, IsComplete, and Priority

---
+++ Modules/ToDo/Task.cs
@@ -0,0 +1,12 @@
+/* Modules/ToDo/Task.cs */
+
+public class Task
+{
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public bool IsComplete { get; private set; }
+    public int Priority { get; private set; }
+
+    
+
+}
