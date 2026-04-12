599eead4ee8368d5f0003333b07749b128c1393e
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-12 16:21:39 -0300

    Fix typo in ITaskManager interface method name from 'DeteleTask' to 'DeleteTask'

--- Modules/ToDo/ITaskManager.cs
+++ Modules/ToDo/ITaskManager.cs
@@ -1,5 +1,3 @@
-/* Modules/ToDo/ITaskManager.cs */
-
 namespace Paperless.Modules.ToDo;
 
 public interface ITaskManager
@@ -7,6 +5,6 @@ public interface ITaskManager
    TodoTask CreateTask(string title, string description, int priority);
    List<TodoTask> ListTasks(int? priorityFilter = null);
    TodoTask? CompleteTask(string id);
-   bool DeteleTask(string id);
+   bool DeleteTask(string id);
    TodoTask? GetTask(string id);
 }
