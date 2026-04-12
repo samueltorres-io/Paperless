b319e31902740ad8d6ba8eb43e40c273cee46bb9
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-12 15:44:03 -0300

    Add file path handling and JSON serialization options in TodoRepository class

--- Modules/ToDo/TodoRepository.cs
+++ Modules/ToDo/TodoRepository.cs
@@ -1,6 +1,32 @@
 /* Modules/ToDo/TodoRepository.cs */
 
+namespace Paperless.Modules.ToDo;
+
 public class TodoRepository
 {
-    
+    private readonly string _filepath;
+
+    private static readonly JsonSerializerOptions JsonOptions = new()
+    {
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.Never
+    };
+
+    public TodoRepository(string filepath)
+    {
+        _filepath = filepath;
+        EnsureFileExists();
+    }
+
+    private void EnsureFileExists()
+    {
+        var directory = Path.GetDirectoryName(_filepath);
+
+        if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+            Directory.CreateDirectory(directory);
+        
+        if (!File.Exists(_filePath))
+            File.WriteAllText(_filePath, "[]");
+    }
 }
