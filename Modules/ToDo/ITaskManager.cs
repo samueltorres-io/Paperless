860d294aa91f45ecb9e614002ba0a568f3e7458a
samueltorres-io <torresfernandessamuel@gmail.com>
2026-04-12 17:56:02 -0300

    Update CreateTask method in ITaskManager interface to accept nullable description and priority parameters for improved flexibility.

--- Modules/ToDo/ITaskManager.cs
+++ Modules/ToDo/ITaskManager.cs
@@ -2,7 +2,7 @@ namespace Paperless.Modules.ToDo;
 
 public interface ITaskManager
 {
-   TodoTask CreateTask(string title, string description, int priority);
+   TodoTask CreateTask(string title, string? description, int? priority);
    List<TodoTask> ListTasks(int? priorityFilter = null);
    TodoTask? CompleteTask(string id);
    bool DeleteTask(string id);
